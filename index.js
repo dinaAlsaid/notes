50feda244d48cef00370356906a5e24eeb3926ba
dinaAlsaid <dinaAlsaid@github.com>
2020-10-13 20:04:15 +0300

    lint pass

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 'use strict';
 
+require('dotenv').config();
+const mongoose = require('mongoose');
 const Notes = require('./lib/notes.js');
 const Input = require('./lib/input.js');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/notedb';
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+
+});
+
+
 
 let usrInput = new Input();
 let note = new Notes(usrInput);
